087905c02c13ad2178edcd54f924af1042cd400b
Luis Silva <luis.silva.prudencio@gmail.com>
2021-05-23 15:47:06 -0500

    video para al cerrar modal

--- R3/scripts.js
+++ R3/scripts.js
@@ -2,11 +2,13 @@ let abrir = document.getElementById("play")
 let cerrar = document.getElementById("cerrar")
 let modal=document.getElementById("ventana")
 let modalCont = document.getElementById("modal-container")
+let video = document.getElementById("video")
 
 abrir.addEventListener("click", ()=>{
     modalCont.style.visibility="visible"
     modalCont.style.opacity="1"
     modal.classList.toggle("modal-cerrado")
+    video.setAttribute("src", "lollapalooza2021.mp4")
 })
 
 cerrar.addEventListener("click", ()=>{
@@ -14,5 +16,6 @@ cerrar.addEventListener("click", ()=>{
     setTimeout(() => {
         modalCont.style.visibility="hidden"
         modalCont.style.opacity="0"
+        video.setAttribute("src", "")
     }, 500);
 })
